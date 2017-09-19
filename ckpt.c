5048efc31a3f16d67e8426a3c24ac39d85c58779
Rongxuan Liu <rongxuanliu@Rongxuans-MBP.fios-router.home>
2017-09-19 12:36:45 -0400

    clang-format using K&R

--- ckpt.c
+++ ckpt.c
@@ -1,17 +1,17 @@
-#include <stdio.h>
+#include <assert.h>
+#include <fcntl.h>
 #include <signal.h>
-#include <sys/types.h>
+#include <stdint.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <fcntl.h>
-#include <stdint.h>
-#include <unistd.h>
+#include <sys/types.h>
 #include <ucontext.h>
-#include <assert.h>
+#include <unistd.h>
 #include "common.h"
 
-#define MAX_MR_PER_PROC 1000    // max number of memory regions in a process
-#define CKPT_NAME "myckpt"		// check point image name
+#define MAX_MR_PER_PROC 1000  // max number of memory regions in a process
+#define CKPT_NAME "myckpt"    // check point image name
 
 // flag
 int flag = 0;
@@ -26,59 +26,59 @@ int write_memory(int fp, int mrsc, struct MemoryRegion *mrs);
 // utility functions
 int get_memory_regions(struct MemoryRegion *mrs);
 
-
-__attribute__ ((constructor))
-void checkpointer() {
-  signal(SIGUSR2, my_handler);
+__attribute__((constructor)) void checkpointer()
+{
+    signal(SIGUSR2, my_handler);
 }
 
-void my_handler(int sig) {
-	signal(sig, SIG_DFL);       // react with default behavior
-	int catch_result = catch_a_check();
-    if (catch_result == 0)
-		return;
-	return;
+void my_handler(int sig)
+{
+    signal(sig, SIG_DFL);  // react with default behavior
+    int catch_result = catch_a_check();
+    if (catch_result == 0) return;
+    return;
 }
 
 /*
  * return 0 if failed to catch
  * return 1 if succeeded
  */
-int catch_a_check() {
-    struct MemoryRegion *mrs;   // memory regions, to be saved
-    int mrs_count;              // number of mr(s), to be saved
-    ucontext_t uc;            	// the context of the calling thread, to be saved
-	int register_res;           // result of getcontext
-	int write_res;				// result of writing image
+int catch_a_check()
+{
+    struct MemoryRegion *mrs;  // memory regions, to be saved
+    int mrs_count;             // number of mr(s), to be saved
+    ucontext_t uc;             // the context of the calling thread, to be saved
+    int register_res;          // result of getcontext
+    int write_res;             // result of writing image
 
     // get memory regions
-    mrs = (struct MemoryRegion *) malloc(MAX_MR_PER_PROC * sizeof(struct MemoryRegion));
+    mrs = (struct MemoryRegion *)malloc(MAX_MR_PER_PROC *
+                                        sizeof(struct MemoryRegion));
     mrs_count = get_memory_regions(mrs);
 
     if (mrs_count == 0) {
-		printf("Failed to read memory regions.\n");  // just be sure
-		return 0;
+        printf("Failed to read memory regions.\n");  // just be sure
+        return 0;
     }
 
-	// get registersf
-	register_res = getcontext(&uc);
+    // get registersf
+    register_res = getcontext(&uc);
 
-	if (flag == 1)
-		return 0;
-	flag = 1;
+    if (flag == 1) return 0;
+    flag = 1;
 
     if (register_res != 0) {
-		printf("Failed to get context.\n");
-		return 0;
+        printf("Failed to get context.\n");
+        return 0;
     }
 
-	// write it
-	write_res = put_a_check(mrs, mrs_count, &uc);
-	if (write_res == 0) {
-		printf("Failed to write image.\n");
-		return 0;
-	}
-	printf("Checkpoint image created.\n");
+    // write it
+    write_res = put_a_check(mrs, mrs_count, &uc);
+    if (write_res == 0) {
+        printf("Failed to write image.\n");
+        return 0;
+    }
+    printf("Checkpoint image created.\n");
     return 1;
 }
 
@@ -86,67 +86,66 @@ int catch_a_check() {
  * return 0 if failed
  * return 1 if successfully written
  */
-int put_a_check(struct MemoryRegion *mrs, int mrsc, ucontext_t *ucp) {
-	int wfp;					// file descriptor of the image file
-	int w_res;					// writing result
+int put_a_check(struct MemoryRegion *mrs, int mrsc, ucontext_t *ucp)
+{
+    int wfp;    // file descriptor of the image file
+    int w_res;  // writing result
 
-	wfp = open(CKPT_NAME, O_CREAT|O_WRONLY|O_TRUNC, S_IRWXU);
-	if (wfp < 0)
-		return 0;
+    wfp = open(CKPT_NAME, O_CREAT | O_WRONLY | O_TRUNC, S_IRWXU);
+    if (wfp < 0) return 0;
 
-	// write context
-	w_res = write_context(wfp, ucp);
-	if (w_res == 0)
-		return 0;
+    // write context
+    w_res = write_context(wfp, ucp);
+    if (w_res == 0) return 0;
 
-	// write memory meta & actual
-	w_res += write_memory(wfp, mrsc, mrs);
-	if (w_res == 0)
-		return 0;
+    // write memory meta & actual
+    w_res += write_memory(wfp, mrsc, mrs);
+    if (w_res == 0) return 0;
 
-	// close the file descriptor
-	int res = close(wfp);
-	if (res < 0)
-		return 0;
+    // close the file descriptor
+    int res = close(wfp);
+    if (res < 0) return 0;
 
-	return 1;
+    return 1;
 }
 
 /*
  * return 0 if failed
  */
-int write_context(int fp, ucontext_t *ucp) {
-	int n = write(fp, ucp, sizeof(ucontext_t));
-	if (n < 0) {
-		printf("Failed to write context\n");
-		exit(1);
-	}
-	return n;
+int write_context(int fp, ucontext_t *ucp)
+{
+    int n = write(fp, ucp, sizeof(ucontext_t));
+    if (n < 0) {
+        printf("Failed to write context\n");
+        exit(1);
+    }
+    return n;
 }
 
 /*
  * return 0 if failed
  */
-int write_memory(int fp, int mrsc, struct MemoryRegion *mrs) {
-	int i;
-	int n;
-	n = 0;
-	for (i=0; i<mrsc; i++) {
-		struct MemoryRegion mr = mrs[i];
-		if (strstr(mr.name, "vsyscall") ||
-			(!mr.isReadable && !mr.isWriteable && !mr.isExecutable)) {
-				continue;
-		}
-		// write meta
-		ssize_t ret = write(fp, &mr, sizeof(mr));
+int write_memory(int fp, int mrsc, struct MemoryRegion *mrs)
+{
+    int i;
+    int n;
+    n = 0;
+    for (i = 0; i < mrsc; i++) {
+        struct MemoryRegion mr = mrs[i];
+        if (strstr(mr.name, "vsyscall") ||
+            (!mr.isReadable && !mr.isWriteable && !mr.isExecutable)) {
+            continue;
+        }
+        // write meta
+        ssize_t ret = write(fp, &mr, sizeof(mr));
         n += ret;
         assert(ret == sizeof(mr));
-		// write actual
-		ret = write(fp, mr.startAddr, (mr.endAddr - mr.startAddr));
+        // write actual
+        ret = write(fp, mr.startAddr, (mr.endAddr - mr.startAddr));
         assert(ret == mr.endAddr - mr.startAddr);
         n += ret;
-	}
-	return n;
+    }
+    return n;
 }
 
 /*
@@ -154,31 +153,31 @@ int write_memory(int fp, int mrsc, struct MemoryRegion *mrs) {
  * return 0 when reading error
  * return the number of memory regions when all good
  */
-int get_memory_regions(struct MemoryRegion *mrs) {
-    int rfp;					// file descriptor for reading
-    int count;                  // memory region count
-    char *rfpath;				// file path for read
-    struct MemoryRegion *pmr;	// the pointer to the currently read memory region
+int get_memory_regions(struct MemoryRegion *mrs)
+{
+    int rfp;       // file descriptor for reading
+    int count;     // memory region count
+    char *rfpath;  // file path for read
+    struct MemoryRegion
+        *pmr;  // the pointer to the currently read memory region
 
-	// get memory maps file path
-	rfpath = "/proc/self/maps";
+    // get memory maps file path
+    rfpath = "/proc/self/maps";
 
-	// get memory maps file descriptor
-	rfp = open(rfpath, O_RDONLY);
-	if (rfp < 0)
-		return 0;
+    // get memory maps file descriptor
+    rfp = open(rfpath, O_RDONLY);
+    if (rfp < 0) return 0;
 
     // read memoery regions
     count = 0;
-	while ((pmr = get_line(rfp))) {
+    while ((pmr = get_line(rfp))) {
         mrs[count] = (*pmr);
         ++count;
-	}
+    }
 
-	// close the file descriptor
-	int res = close(rfp);
-	if (res < 0)
-		return 0;
+    // close the file descriptor
+    int res = close(rfp);
+    if (res < 0) return 0;
 
     return count;
 }
--- restart.c
+++ restart.c
@@ -1,21 +1,21 @@
+#include <errno.h>
+#include <fcntl.h>
+#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <signal.h>
+#include <string.h>
 #include <sys/mman.h>
 #include <sys/types.h>
-#include <fcntl.h>
 #include <ucontext.h>
-#include <string.h>
-#include <errno.h>
 #include "common.h"
 
 #define MAP_ADDR 0x5300000
 #define MAP_LEN 0x100000
 
 // global variables
-char filename_g[1000];                          // filename from user input
-int ckptfp;										// file descriptor for the image file
-ucontext_t uc_g;                                // context to be continued
+char filename_g[1000];  // filename from user input
+int ckptfp;             // file descriptor for the image file
+ucontext_t uc_g;        // context to be continued
 struct MemoryRegion mr_g;
 
 // main processes
@@ -31,145 +31,145 @@ int locate_old_stack(struct MemoryRegion *pmr);
 
 int main(int argc, char **argv)
 {
-	void *pmap, *emap;
+    void *pmap, *emap;
 
-    if (argc == 1)
-        return 0;
-	sprintf(filename_g, "%s", argv[1]);
+    if (argc == 1) return 0;
+    sprintf(filename_g, "%s", argv[1]);
 
-	// map memory
-	pmap = map_memory((void *) MAP_ADDR);
-	emap = pmap + MAP_LEN;
-    if (pmap == (void *) -1)
-		return 0;
-	// swtich stack pointer
-	asm volatile ("mov %0,%%rsp" : : "g" (emap) : "memory");
-	unmap_and_restore();
+    // map memory
+    pmap = map_memory((void *)MAP_ADDR);
+    emap = pmap + MAP_LEN;
+    if (pmap == (void *)-1) return 0;
+    // swtich stack pointer
+    asm volatile("mov %0,%%rsp" : : "g"(emap) : "memory");
+    unmap_and_restore();
 }
 
 /*
  * return 0 when failed to unmap
  */
-void unmap_and_restore() {
+void unmap_and_restore()
+{
     int fp, read_res, res;
     char filename[1000];
-	struct MemoryRegion *pmr;
+    struct MemoryRegion *pmr;
 
-	unsigned long long length;
+    unsigned long long length;
 
     fp = get_fdescriptor("/proc/self/maps");
-    if (fp < 0)
+    if (fp < 0) exit(1);
+
+    while ((pmr = get_line(fp))) {
+        if (locate_old_stack(pmr)) break;
+    }
+
+    res = close(fp);
+    if (res < 0) {
+        printf("Failed closing current process map (error:%s)\n",
+               strerror(errno));
         exit(1);
+    }
 
-	while((pmr = get_line(fp))) {
-		if (locate_old_stack(pmr))
-			break;
-	}
+    length = pmr->endAddr - pmr->startAddr;
+    res = munmap(pmr->startAddr, length);
 
-	res = close(fp);
-	if (res < 0) {
-		printf("Failed closing current process map (error:%s)\n", strerror(errno));
-		exit(1);
-	}
+    ckptfp = get_fdescriptor(filename_g);
+    if (ckptfp < 0) exit(1);
 
-	length = pmr->endAddr - pmr->startAddr;
-	res = munmap(pmr->startAddr, length);
+    // restore previous memory
+    read_res = read_context(ckptfp);
+    read_res += restore_memory(ckptfp);
 
-	ckptfp = get_fdescriptor(filename_g);
-	if (ckptfp < 0)
-		exit(1);
-
-	// restore previous memory
-	read_res = read_context(ckptfp);
-	read_res +=	restore_memory(ckptfp);
-
-	// revoke
-	revoke_context();
-	return;
+    // revoke
+    revoke_context();
+    return;
 }
 
-void revoke_context() {
-	int res;
-	// close checkpoint image
-	res = close(ckptfp);
-	if (res < 0) {
-		printf("Failed closing current process map (error:%s).\n", strerror(errno));
-		exit(1);
-	}
-	// restart
-	res = setcontext(&uc_g);
-	if (res < 0) {
-		printf("Restore failed (error:%s).\n", strerror(errno));
-		exit(1);
-	}
+void revoke_context()
+{
+    int res;
+    // close checkpoint image
+    res = close(ckptfp);
+    if (res < 0) {
+        printf("Failed closing current process map (error:%s).\n",
+               strerror(errno));
+        exit(1);
+    }
+    // restart
+    res = setcontext(&uc_g);
+    if (res < 0) {
+        printf("Restore failed (error:%s).\n", strerror(errno));
+        exit(1);
+    }
 }
 
 /*
  * return 1 when successfully unmapped
  */
-int locate_old_stack(struct MemoryRegion *pmr) {
-	if (strcmp(pmr->name, "[stack]") == 0)
-		return 1;
-	return 0;
+int locate_old_stack(struct MemoryRegion *pmr)
+{
+    if (strcmp(pmr->name, "[stack]") == 0) return 1;
+    return 0;
 }
 
-int restore_memory(int fp) {
+int restore_memory(int fp)
+{
     struct MemoryRegion mr;
-	int count, n, ir, iw, ie, ip, mprot_res;
-	unsigned long long sai, eai, length;
-	void *sa, *ea, *mapped;
-	count = 0;
+    int count, n, ir, iw, ie, ip, mprot_res;
+    unsigned long long sai, eai, length;
+    void *sa, *ea, *mapped;
+    count = 0;
 
-	// pre-read the next start address as test scenario
-	while((n = read(fp, &mr, sizeof(mr))) > 0) {
-		length = mr.endAddr - mr.startAddr;
-		// load the memory region
-		mapped = mmap(mr.startAddr, length, PROT_READ | PROT_WRITE | PROT_EXEC,
-                MAP_ANONYMOUS | MAP_FIXED | MAP_PRIVATE, -1, 0);
-		if (mapped == (void *) -1) {
-			printf("Error when mapping memory region %p %p (error:%s)\n", sa, ea, strerror(errno));
-			break;
-		}
-		count += n = read(fp, mr.startAddr, length);
-		if (n < 0)
-			break;
-		// set the permission
-		int permissions = 0;
-		if (mr.isReadable)
-			permissions |= PROT_READ;
-		if (mr.isWriteable)
-			permissions |= PROT_WRITE;
-		if (mr.isExecutable)
-			permissions |= PROT_EXEC;
-		mprot_res = mprotect(mr.startAddr, length, permissions);
-		if (mprot_res < 0) {
-			printf("Error when setting protection on memory region %p %p (error:%s)\n",
-                    mr.startAddr, mr.endAddr, strerror(errno));
-			break;
-		}
-	}
-	if (n < 0) {
-		printf("Failed to restore memory\n");
-		exit(1);
-	}
-	return count;
+    // pre-read the next start address as test scenario
+    while ((n = read(fp, &mr, sizeof(mr))) > 0) {
+        length = mr.endAddr - mr.startAddr;
+        // load the memory region
+        mapped = mmap(mr.startAddr, length, PROT_READ | PROT_WRITE | PROT_EXEC,
+                      MAP_ANONYMOUS | MAP_FIXED | MAP_PRIVATE, -1, 0);
+        if (mapped == (void *)-1) {
+            printf("Error when mapping memory region %p %p (error:%s)\n", sa,
+                   ea, strerror(errno));
+            break;
+        }
+        count += n = read(fp, mr.startAddr, length);
+        if (n < 0) break;
+        // set the permission
+        int permissions = 0;
+        if (mr.isReadable) permissions |= PROT_READ;
+        if (mr.isWriteable) permissions |= PROT_WRITE;
+        if (mr.isExecutable) permissions |= PROT_EXEC;
+        mprot_res = mprotect(mr.startAddr, length, permissions);
+        if (mprot_res < 0) {
+            printf(
+                "Error when setting protection on memory region %p %p "
+                "(error:%s)\n",
+                mr.startAddr, mr.endAddr, strerror(errno));
+            break;
+        }
+    }
+    if (n < 0) {
+        printf("Failed to restore memory\n");
+        exit(1);
+    }
+    return count;
 }
 
-
-
-int read_context(int fp) {
-	int n = read(fp, &uc_g, sizeof(ucontext_t));
-	if (n < 0) {
-		printf("Error reading context from image (error:%s)\n", strerror(errno));
-		exit(1);
-	}
+int read_context(int fp)
+{
+    int n = read(fp, &uc_g, sizeof(ucontext_t));
+    if (n < 0) {
+        printf("Error reading context from image (error:%s)\n",
+               strerror(errno));
+        exit(1);
+    }
     return n;
 }
 
 /*
  * return file descriptor
  */
-int get_fdescriptor(char *filename) {
+int get_fdescriptor(char *filename)
+{
     int fp = open(filename, O_RDONLY);
     return fp;
 }
@@ -177,12 +177,8 @@ int get_fdescriptor(char *filename) {
 /*
  * return * to the mapped area
  */
-void *map_memory(void *addr) {
-    return mmap(
-        addr,
-        MAP_LEN,
-        PROT_READ | PROT_WRITE | PROT_EXEC, MAP_ANONYMOUS | MAP_FIXED | MAP_PRIVATE,
-        -1,
-        0
-    );
+void *map_memory(void *addr)
+{
+    return mmap(addr, MAP_LEN, PROT_READ | PROT_WRITE | PROT_EXEC,
+                MAP_ANONYMOUS | MAP_FIXED | MAP_PRIVATE, -1, 0);
 }
